69a06efe6223642f1681a4f723c53cf47bc59b80
Jonathan Ruiz <jeruiz@mpcomunas.gob.ve>
2021-12-21 11:10:04 -0400

    configurando Stack

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+
+const App = () => {
+  return (
+
+    <NavigationContainer>
+        <Text>App Screen</Text>
+    </NavigationContainer>
+
+
+    
+  )
+}
+
+export default App;
